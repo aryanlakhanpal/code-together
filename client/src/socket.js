6b9f0a4585b0c0eb6900f8c1546b0c942a5ab1e4
Aryan lakhanpal <123261498+aryanlakhanpal@users.noreply.github.com>
2025-01-24 21:03:20 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -7,5 +7,5 @@ export const initSocket = async () => {
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io(process.env.REACT_APP_BACKEND_URL || "https://simultaneouscodeeditor-production.up.railway.app", options);
+    return io(process.env.REACT_APP_BACKEND_URL || "code-together-production.up.railway.app", options);
 };
